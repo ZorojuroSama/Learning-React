ec8de981bfda3bb3c4c82ac3b1194463e0b31f25
Shubh <sahilraj2310@gmail.com>
2024-08-23 23:11:29 +0530

    turututruu

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./Chai";
+
+
+function App() {
+
+  const username = 'Shubh'
+
+  return (
+    <>
+  <Chai/>
+  <h1>Chai aur react with {username}</h1>
+  <p>test para</p>
+    </>
+  )
+}
+
+export default App;
