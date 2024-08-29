2e9df0eb2f831188e2f4145b9b66fdcf31ffc3f2
Shubh <sahilraj2310@gmail.com>
2024-08-29 22:06:26 +0530

    jkn

--- 03tailwindProps/src/components/Card.jsx
+++ 03tailwindProps/src/components/Card.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 function Card({username, btnText}) {
   return (
-    <>
       <div className="relative h-[400px] w-[300px] rounded-md">
         <img
           src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
@@ -21,7 +20,6 @@ function Card({username, btnText}) {
           </button>
         </div>
       </div>
-    </>
   );
 }
 
