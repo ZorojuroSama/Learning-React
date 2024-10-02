917166476e276ca68ae8dfc9b48c9e97a353be1b
Shubh <sahilraj2310@gmail.com>
2024-10-02 17:02:39 +0530

    little css changes

--- 10ToDoApp/src/App.jsx
+++ 10ToDoApp/src/App.jsx
@@ -53,8 +53,8 @@ function App() {
   return (
     <TodoProvider
       value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
-      <div className="bg-[#172842] min-h-screen py-8">
-        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+      <div className="bg-[#152e52] min-h-screen py-2">
+        <div className="w-full max-w-2xl mx-auto rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
